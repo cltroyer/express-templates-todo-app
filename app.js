20b573d6ceedd45cc75c26948cf6788341935f03
Lachlan Heywood <lachlan@heywood.net.au>
2021-01-05 17:40:52 -0500

    prepare for template engine

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const todoListData = require('./todoListData');
 
+// App Setup
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static('./public'));
 
-const todoList = [
-  {
-    id: 1,
-    todo: 'Implement a REST API',
-  },
-  {
-    id: 2,
-    todo: 'Build a frontend',
-  },
-  {
-    id: 3,
-    todo: '???',
-  },
-  {
-    id: 4,
-    todo: 'Profit!',
-  },
-];
+// Template Engine Configuration
 
-// GET /api/todos
 
-// GET /api/todos/:id
 
-// POST /api/todos
+// GET /todos
 
-// PUT /api/todos/:id
+// GET /todos/:id
 
-// DELETE /api/todos/:id
+// POST /todos
+
+// PUT /todos/:id
+
+// DELETE /todos/:id
 
 app.listen(3000, function () {
-  console.log('Todo List API is now listening on port 3000...');
+  console.log('Todo List App is now listening on port 3000...');
 });
---
+++ todoListData.js
@@ -0,0 +1,20 @@
+const todoListData = [
+  {
+    id: 1,
+    content: 'Implement a REST API',
+  },
+  {
+    id: 2,
+    content: 'Build a frontend',
+  },
+  {
+    id: 3,
+    content: '???',
+  },
+  {
+    id: 4,
+    content: 'Profit!',
+  },
+];
+
+module.exports = todoListData;
